ebcefed2a8e6dffde8dfcc793b718b20d69ab7fc
Amit <amitbd590@gmail.com>
2021-07-13 02:51:33 +0600

    1st

--- js/main.js
+++ js/main.js
@@ -1,26 +1,12 @@
-let datas = [10, 50, 30, 70, 45, 10];
-    i = 0;
-    instance = 0;
-    elem = "#1"
-    setInterval(function() {
-        let path = $(elem).find("path"),
-            prodata = $(elem).find("[data-func='prodata']");
-        //dataStart = (dataStart / dataMark) * 100;
-        dataStart = path.attr('data-point') ? $(elem).find("path").attr('data-point') : 0;
-        dataMark = prodata.attr('data-mark');
-        dataStop = datas[i];
-        prodata.attr({
-            "data-start": dataStart,
-            "data-end": dataStop
-        });
-        console.log(dataStart)
-        setTimeout(function() {
-            autoRatePoint("#1")
-        }, 200);
-        if (i == datas.length) {
-            i = 0;
-            instance = 0;
-        }
-        i++;
-        instance = 1;
-}, 10000)
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.owl-prev').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.owl-next').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
