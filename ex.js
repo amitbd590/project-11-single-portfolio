7c6cb3bfff26736e174d1641a9a0c5b062df855c
Amit <amitbd590@gmail.com>
2021-07-16 03:23:52 +0600

    text

---
+++ ex.js
@@ -0,0 +1,25 @@
+
+(function ($) {
+    'use strict';
+  
+  jQuery(document).ready(function($) {
+  
+      
+
+
+    
+  
+  
+   });
+      
+  
+      //jquery load function start
+      jQuery(window).on("load", function() {
+          
+      });
+  
+  
+  }(jQuery));
+  
+  
+  {"mode":"full","isActive":false}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel();
-// Go to the next item
-$('.owl-prev').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-// Go to the previous item
-$('.owl-next').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [300]);
-})
+
+    var forNew = $('.portfolio');
+    forNew.owlCarousel();
+    // Go to the next item
+    $('.owl-prev').click(function () {
+      forNew.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.owl-next').click(function () {
+      // With optional speed parameter
+      // Parameters has to be in square bracket '[]'
+      forNew.trigger('prev.owl.carousel', [300]);
+    })
+
+
+    var blog = $(".blog");
+    blog.owlCarousel({
+      loop: true,
+      nav: true,
+      dots: true,
+      animateIn: 'fadeInRight',
+      animateOut: 'fadeInRight',
+      autoplay: true,
+      items: 3,
+
+    });
+    $('.owl-prev').click(function () {
+      blog.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.owl-next').click(function () {
+      // With optional speed parameter
+      // Parameters has to be in square bracket '[]'
+      blog.trigger('prev.owl.carousel', [300]);
+    })
+
+
+
+
+
+
